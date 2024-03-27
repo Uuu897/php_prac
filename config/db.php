26fa899188815843491c6c3db929e9ad44e89409
Uuu897 <Nastiushakova@yandex.ru>
2024-03-27 14:10:10 +0700

    задание 2

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'vgzuyrud_m1',
+    'username' => 'vgzuyrud',
+    'password' => 'vh84MG',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
