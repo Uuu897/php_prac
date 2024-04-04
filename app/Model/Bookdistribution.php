13718cfe6b4c442b0823105860550a4c0257ebfb
Uuu897 <Nastiushakova@yandex.ru>
2024-04-04 16:30:22 +0700

    фиик8

--- app/Model/Bookdistribution.php
+++ app/Model/Bookdistribution.php
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace Model;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Bookdistribution extends Model
-{
-    use HasFactory;
-
-    public $timestamps = false;
-    protected $table = 'book_distribution';
-    protected $fillable = [
-        'id_book' ,
-        'id_reader' ,
-        'loan_date' ,
-        'return_date' ,
-        'status' ,
-    ];
-=======
 <?php
 
 namespace Model;
@@ -40,5 +18,5 @@ class Bookdistribution extends Model
         'return_date',
         'status'
     ];
->>>>>>> d152504 (фиик)
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
 <?php
-
-use Src\Route;
-
-Route::add(['GET', 'POST'], '/hello', [Controller\Site::class, 'hello'])
-    ->middleware('auth');
-Route::add(['GET', 'POST'], '/signup', [Controller\Site::class, 'signup']);
-Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
-Route::add(['GET', 'POST'], '/logout', [Controller\Site::class, 'logout']);
-Route::add('GET', '/popular', [Controller\Site::class, 'popular']);
-Route::add('GET', '/history', [Controller\Site::class, 'history']);
-Route::add(['GET', 'POST'], '/add_reader', [Controller\Site::class, 'add_reader']);
-Route::add(['GET', 'POST'], '/add_books', [Controller\Site::class, 'add_books']);
-Route::add(['GET', 'POST'], '/add_librarian', [Controller\Site::class, 'add_librarian']);
-=======
-<?php
-
 use Src\Route;
 
 Route::add(['GET', 'POST'], '/hello', [Controller\Site::class, 'hello'])
@@ -29,4 +12,3 @@ Route::add(['GET', 'POST'], '/add_reader', [Controller\Site::class, 'add_reader'
 Route::add(['GET', 'POST'], '/add_books', [Controller\Site::class, 'add_books']);
 Route::add(['GET', 'POST'], '/add_librarian', [Controller\Site::class, 'add_librarian']);
 Route::add(['GET', 'POST'], '/search', [Controller\Site::class, 'search']);
->>>>>>> d152504 (фиик)
